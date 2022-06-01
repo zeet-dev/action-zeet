05c0b02dd4b3b77dc1cccb66405171480eba4a13
hi019 <65871571+hi019@users.noreply.github.com>
2022-06-01 19:41:36 -0400

    Add --overwrite to login

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function run() {
     await exec.exec('zeet', ['config:set', `server=${apiURL}`]);
 
     const token = core.getInput('token', { required: true });
-    await exec.exec('zeet', ['login', `--token=${token}`]);
+    await exec.exec('zeet', ['login', '--overwrite', `--token=${token}`]);
 
     core.info("Done!");
   } catch (error) {
